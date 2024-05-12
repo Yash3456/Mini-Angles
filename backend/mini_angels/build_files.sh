36971574e4c3849039135d8fe47867268c7d7f76
Lokeshwaran Boopathy <lokeshwaran.boopathy@oracle.com>
2024-05-12 16:20:16 +0530

    Testing

--- backend/mini_angels/build_files.sh
+++ backend/mini_angels/build_files.sh
@@ -4,22 +4,22 @@
 
 # yum install -y mysql
 
-yum remove -y python3.12
+# yum remove -y python3.12
 
-yum install -y wget
+# yum install -y wget
 
-yum install gcc openssl-devel zlib-devel ncurses-devel readline-devel sqlite-devel \
-    gdbm-devel libdb-devel libbz2-devel expat-devel libffi-devel xz-devel tk-devel
+# yum install gcc openssl-devel zlib-devel ncurses-devel readline-devel sqlite-devel \
+#     gdbm-devel libdb-devel libbz2-devel expat-devel libffi-devel xz-devel tk-devel
 
 
-wget https://www.python.org/ftp/python/3.12.0/Python-3.12.0.tar.xz
-./configure --enable-loadable-sqlite-extensions --enable-optimizations
-make
-make install
+# wget https://www.python.org/ftp/python/3.12.0/Python-3.12.0.tar.xz
+# ./configure --enable-loadable-sqlite-extensions --enable-optimizations
+# make
+# make install
 
-yum update
+# yum update
 
-yum install -y python3-pip sqlite-devel
+yum install -y sqlite-devel
 
 pip3 install --upgrade pip
 
@@ -27,6 +27,6 @@ pip3 install --upgrade --force-reinstall -r requirements.txt
 
 # sed -i 's/.*_sqlite3.*//g' /python312/lib/python3.12/sqlite3/dbapi2.py
 
-python3 manage.py makemigrations --noinput
+python3 manage.py makemigrations
 
-python3 manage.py migrate --noinput
+python3 manage.py migrate
