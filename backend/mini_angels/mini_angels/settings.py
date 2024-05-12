27382bfad653e3c39e196004bcec9c137c80971f
Lokeshwaran Boopathy <lokeshwaran.boopathy@oracle.com>
2024-05-12 13:51:22 +0530

    Fixed DB issue

--- backend/mini_angels/mini_angels/settings.py
+++ backend/mini_angels/mini_angels/settings.py
@@ -77,13 +77,13 @@ WSGI_APPLICATION = 'mini_angels.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+DATABASES = {}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
