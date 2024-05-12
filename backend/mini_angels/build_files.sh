b4f1b4fc7b4abb5e465f8a670e11670f91553ae9
Lokeshwaran Boopathy <lokeshwaran.boopathy@oracle.com>
2024-05-12 16:05:22 +0530

    Testing

--- backend/mini_angels/build_files.sh
+++ backend/mini_angels/build_files.sh
@@ -5,6 +5,8 @@
 # yum install -y mysql
 pip3 install --upgrade pip
 
+yum install -y sqlite-devel
+
 pip3 install --upgrade --force-reinstall -r requirements.txt
 
 # sed -i 's/.*_sqlite3.*//g' /python312/lib/python3.12/sqlite3/dbapi2.py
