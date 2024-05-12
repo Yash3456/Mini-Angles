a7b1a3402cea7c54d52b7763962d3e8c227d8050
Lokeshwaran Boopathy <lokeshwaran.boopathy@oracle.com>
2024-05-12 11:24:20 +0530

    Made backend code improvements

--- backend/mini_angels/borrower/urls.py
+++ backend/mini_angels/borrower/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('verify_mobile/', views.verify_mobile, name='verify_mobile'),
     path('verify_otp/', views.verify_otp, name='verify_otp'),
     path('save_profile/', views.save_profile, name='save_profile'),
+    path('loan_application/', views.loan_application, name='loan_application'),
 ]
