58ba054044270142b9edd534547404e28958dd18
Lokeshwaran Boopathy <lokeshwaran.boopathy@oracle.com>
2024-05-12 16:21:36 +0530

    Testing

--- backend/mini_angels/build_files.sh
+++ backend/mini_angels/build_files.sh
@@ -4,18 +4,18 @@
 
 # yum install -y mysql
 
-# yum remove -y python3.12
+yum remove -y python3.12
 
-# yum install -y wget
+yum install -y wget
 
 # yum install gcc openssl-devel zlib-devel ncurses-devel readline-devel sqlite-devel \
 #     gdbm-devel libdb-devel libbz2-devel expat-devel libffi-devel xz-devel tk-devel
 
 
-# wget https://www.python.org/ftp/python/3.12.0/Python-3.12.0.tar.xz
-# ./configure --enable-loadable-sqlite-extensions --enable-optimizations
-# make
-# make install
+wget https://www.python.org/ftp/python/3.12.0/Python-3.12.0.tar.xz
+./configure --enable-optimizations --with-ensurepip=install
+make
+make install
 
 # yum update
 
